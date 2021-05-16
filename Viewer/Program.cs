36486e5814a67635f4facf1cfa5c88bddd05d748
DeadEuclid <60969774+DeadEuclid@users.noreply.github.com>
2021-05-16 21:33:56 +0700

    Формы починены

--- Viewer/Program.cs
+++ Viewer/Program.cs
@@ -16,7 +16,7 @@ namespace Viewer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AddForm());
+            Application.Run(new MainForm());
 
         }
     }
